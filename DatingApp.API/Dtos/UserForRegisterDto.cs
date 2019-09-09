7e896c0901b49fe3203176d496ba336c0add2094
Sachin Mahandule <sachinmahandule03@gmail.com>
2019-09-09 08:30:58 +0530

    End of Section 12 and 13

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,14 +1,35 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
         [Required]
         public string Username { get; set; }
 
         [Required]
         [StringLength(25, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 25 characters")]
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+
+
     }
 }
